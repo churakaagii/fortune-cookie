68f35a20fa4e572bf63d792a606ce25aa4d14c1f
churakaagii <a.r.nakama@gmail.com>
2017-01-20 19:20:26 -0500

    added improved fortunes

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ def makep(string):
 def genFortune():
     flist = [
         "You will die tomorrow.",
-        "You will fight a bear and lose.",
-        "You will fight a bear and win.",
-        "You will meet Toilet Hanako.",
-        "You will be smooched by a ghost.",
-        "You will stop asking the internet to tell your future.",
-        "You will go to space.",
-        "You will live peacefully in Okinawa and everyone will love you."
+        "Do not fight a bear; you will lose.",
+        "Absolutely fight a bear; you will win and it will be super cool.",
+        "When next you pee, you will meet Toilet Hanako.",
+        "Always smooch ghosts.",
+        "Stop asking the internet to tell your future.",
+        "The future is in plastics.",
+        "You will live peacefully in Okinawa and everyone will love you.",
+        "Your true love is in Beirut.",
+        "If you steal the Declaration of Independence, you will not find treasure; you will get arrested.",
+        "The next time you look for something lost, it will be in the fridge.",
+        "You should marry the next person who asks you the time."
     ]
 
     return flist[random.randrange(len(flist))]
