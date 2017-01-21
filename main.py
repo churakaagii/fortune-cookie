ef78467bda4a06d503fc833e30e39715dcde06db
churakaagii <a.r.nakama@gmail.com>
2017-01-20 19:13:46 -0500

    fortune cookie app complete!

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
-import webapp2
+import webapp2, random
+
+def makep(string):
+    return "<p>" + string + "</p>"
+    
+def genFortune():
+    flist = [
+        "You will die tomorrow.",
+        "You will fight a bear and lose.",
+        "You will fight a bear and win.",
+        "You will meet Toilet Hanako.",
+        "You will be smooched by a ghost.",
+        "You will stop asking the internet to tell your future.",
+        "You will go to space.",
+        "You will live peacefully in Okinawa and everyone will love you."
+    ]
+
+    return flist[random.randrange(len(flist))]
+
+def makeStrong(string):
+    return "<strong>" + string + "</strong>"
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!!!!')
+        header = "<h1>Fortune Cookie</h1>"
+        
+        fortune = makep("Your fortune: " + makeStrong(genFortune()))
+        
+        luckynum = random.randint(1, 100)
+        luckysen = makep("Your lucky number: " + makeStrong(str(luckynum)))
+        
+        refrbutton = "<a href='.'><button>Try another fortune!</button></a>"
+        
+        content = header + fortune + luckysen + refrbutton
+        self.response.write(content)
+        
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
